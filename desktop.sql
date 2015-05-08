44659d5740a53c8b47e82060531bc38e2389bb80
ironholds <ironholds@gmail.com>
2015-05-08 19:06:42 -0400

    shift query to SQL

---
+++ desktop.sql
@@ -0,0 +1,11 @@
+SELECT timestamp,
+CASE event_action WHEN 'click-result' THEN 'clickthroughs'
+WHEN 'session-start' THEN 'search sessions'
+WHEN 'impression-results' THEN 'Result pages opened' AS action,
+event_clickIndex AS click_index,
+event_numberOfResults AS result_count,
+event_resultSetType as result_type,
+event_timeOffsetSinceStart AS time_offset,
+event_timeToDisplayResults AS
+load_time FROM Search_11670541
+WHERE event_action IN ('click-result','session-start','impression-results');
