4fca1e153b188066f25bb80ea593266d253ff31d
ironholds <ironholds@gmail.com>
2015-05-08 06:09:39 -0400

    expand dygraph

--- server.R
+++ server.R
@@ -1,8 +1,18 @@
 library(readr)
 library(xts)
+library(reshape2)
+library(shinySignals)
+library(RColorBrewer)
 
 shinyServer(function(input, output) {
-  output$desktop_event_plot <- renderDygraph(
-    dygraph(xts(data, data$timestamp), main = "example dygraph", group = "action")
-  )
+  output$desktop_event_plot <- renderDygraph({
+    dygraph_set <- dcast(data, formula = timestamp ~ action)
+    dyOptions(
+      dyLegend(
+        dygraph(xts(dygraph_set[,-1], dygraph_set[,1]), main = "Desktop search events, by day",
+              xlab = "Date", ylab = "Events"),
+        labelsSeparateLines = TRUE
+      ), strokeWidth = 2, colors = brewer.pal(3, "Set2")
+    )
+  })
 })
