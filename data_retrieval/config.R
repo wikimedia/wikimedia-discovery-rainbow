64859578ee7e86cd117d6d02f2081811f9583444
ironholds <ironholds@gmail.com>
2015-05-22 14:45:00 -0400

    of COURSE this is randomly in a newer R version.

--- data_retrieval/config.R
+++ data_retrieval/config.R
@@ -2,12 +2,11 @@
 options(scipen = 500,
         q = "no")
 base_path <- "/a/public-datasets/search/"
-archive_path <- "/a/public-datasets/search/archive"
 
 #Dependencies
 library(olivr)
 suppressPackageStartupMessages(library(data.table))
 
-if(!dir.exists(base_path)){
+if(!file.exists(base_path)){
   dir.create(path = base_path)
 }
