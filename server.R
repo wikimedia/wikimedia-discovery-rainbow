cef3ae7a9d9e7f80bca54da7200cb17bbd3f5085
ironholds <ironholds@gmail.com>
2015-06-19 17:51:32 -0400

    simplify code to avoid environmental nonsense

--- server.R
+++ server.R
@@ -4,20 +4,19 @@ library(reshape2)
 library(RColorBrewer)
 source("utils.R")
 
-data_env <- new.env()
-assign("existing_date", (Sys.Date() -1), envir = data_env)
+existing_date <- (Sys.Date()-1)
 
+#Read in desktop data and generate means for the value boxes, along with a time-series appropriate form for
+#dygraphs.
 read_desktop <- function(){
   data <- download_set("http://datasets.wikimedia.org/aggregate-datasets/search/desktop_event_counts.tsv")
   interim <- reshape2::dcast(data, formula = timestamp ~ action, fun.aggregate = sum)
   interim[is.na(interim)] <- 0
-  assign("desktop_dygraph_set", interim, envir = data_env)
-  assign("desktop_dygraph_means", round(colMeans(get("desktop_dygraph_set", envir = data_env)[,2:4])),
-         envir = data_env)
+  desktop_dygraph_set <<- interim
+  desktop_dygraph_means <<- round(colMeans(desktop_dygraph_set[,2:5]))
   
   data <- download_set("http://datasets.wikimedia.org/aggregate-datasets/search/desktop_load_times.tsv")
-  assign("desktop_load_data", data,
-         envir = data_env)
+  desktop_load_data <<- data
   return(invisible())
 }
 
@@ -25,13 +24,10 @@ read_web <- function(){
   data <- download_set("http://datasets.wikimedia.org/aggregate-datasets/search/mobile_event_counts.tsv")
   interim <- reshape2::dcast(data, formula = timestamp ~ action, fun.aggregate = sum)
   interim[is.na(interim)] <- 0
-  assign("mobile_dygraph_set", interim, envir = data_env)
-  assign("mobile_dygraph_set", reshape2::dcast(data, formula = timestamp ~ action, fun.aggregate = sum), envir = data_env)
-  assign("mobile_dygraph_means", round(colMeans(get("mobile_dygraph_set", envir = data_env)[,2:4])),
-         envir = data_env)
-  con <- url("http://datasets.wikimedia.org/aggregate-datasets/search/mobile_load_times.tsv")
-  assign("mobile_load_data", readr::read_delim(con, delim = "\t"),
-         envir = data_env)
+  mobile_dygraph_set <<- interim
+  mobile_dygraph_means <<- round(colMeans(mobile_dygraph_set[,2:4]))
+  
+  mobile_load_data <<- download_set("http://datasets.wikimedia.org/aggregate-datasets/search/mobile_load_times.tsv")
   return(invisible())
 }
 
@@ -39,42 +35,34 @@ read_apps <- function(){
   data <- download_set("http://datasets.wikimedia.org/aggregate-datasets/search/app_event_counts.tsv")
   interim <- reshape2::dcast(data, formula = timestamp ~ action, fun.aggregate = sum)
   interim[is.na(interim)] <- 0
-  assign("app_dygraph_set", interim, envir = data_env)
-  assign("app_dygraph_means", round(colMeans(get("app_dygraph_set", envir = data_env)[,2:4])),
-         envir = data_env)
-  con <- url("http://datasets.wikimedia.org/aggregate-datasets/search/app_load_times.tsv")
-  assign("app_load_data", readr::read_delim(con, delim = "\t"),
-         envir = data_env)
+  app_dygraph_set <<- interim
+  app_dygraph_means <<- round(colMeans(app_dygraph_set[,2:4]))
+  
+  app_load_data <<- download_set("http://datasets.wikimedia.org/aggregate-datasets/search/app_load_times.tsv")
   return(invisible())
 }
 
 read_failures <- function(date){
-  con <- url("http://datasets.wikimedia.org/aggregate-datasets/search/cirrus_query_aggregates.tsv")
-  data <- readr::read_delim(con, delim = "\t")
+  data <- download_set("http://datasets.wikimedia.org/aggregate-datasets/search/cirrus_query_aggregates.tsv")
   interim_data <- reshape2::dcast(data, formula = date ~ variable, fun.aggregate = sum)
-  assign("failure_dygraph_set", interim_data, envir = data_env)
-  
-#   date <- gsub(x = date, pattern = "-", replacement="")
-#   con <- url(paste0("http://datasets.wikimedia.org/aggregate-datasets/search/daily_pages/zero_resulting_queries",
-#                     date, ".tsv"))
-#   data <- readr::read_delim(con, delim = "\t")
-#   assign("failure_table_set", data, envir = data_env)
+  failure_dygraph_set <<- interim_data
+  return(invisible())
 }
 
 shinyServer(function(input, output) {
   
-  if(Sys.Date() != get("existing_date", envir = data_env)){
+  if(Sys.Date() != existing_date){
     read_desktop()
     read_apps()
     read_web()
-    read_failures(get("existing_date", envir = data_env))
-    assign("existing_date", Sys.Date(), envir = data_env)
+    read_failures(existing_date)
+    existing_date <<- Sys.Date()
   }
   
   #Desktop value boxes
   output$desktop_event_searches <- renderValueBox(
     valueBox(
-      value = get("desktop_dygraph_means", envir = data_env)[3],
+      value = desktop_dygraph_means[4],
       subtitle = "Search sessions per day",
       icon = icon("search"),
       color = "green"
@@ -83,7 +71,7 @@ shinyServer(function(input, output) {
   
   output$desktop_event_resultsets <- renderValueBox(
     valueBox(
-      value = get("desktop_dygraph_means", envir = data_env)[2],
+      value = desktop_dygraph_means[3],
       subtitle = "Result sets per day",
       icon = icon("list", lib = "glyphicon"),
       color = "green"
@@ -92,7 +80,7 @@ shinyServer(function(input, output) {
   
   output$desktop_event_clickthroughs <- renderValueBox(
     valueBox(
-      value = get("desktop_dygraph_means", envir = data_env)[1],
+      value = desktop_dygraph_means[1],
       subtitle = "Clickthroughs per day",
       icon = icon("hand-up", lib = "glyphicon"),
       color = "green"
@@ -101,19 +89,18 @@ shinyServer(function(input, output) {
   
   #The dynamic graphs of events on desktop
   output$desktop_event_plot <- make_dygraph(
-    get("desktop_dygraph_set", envir = data_env), "Date", "Events",
+    desktop_dygraph_set, "Date", "Events",
     "Desktop search events, by day"
   )
   output$desktop_load_plot <- make_dygraph(
-    get("desktop_load_data", envir = data_env), "Date", "Load time (ms)",
+    desktop_load_data, "Date", "Load time (ms)",
     "Desktop result load times, by day"
   )
-  
   
   #Mobile value boxes
   output$mobile_event_searches <- renderValueBox(
     valueBox(
-      value = get("mobile_dygraph_means", envir = data_env)[3],
+      value = mobile_dygraph_means[3],
       subtitle = "Search sessions per day",
       icon = icon("search"),
       color = "green"
@@ -122,7 +109,7 @@ shinyServer(function(input, output) {
   
   output$mobile_event_resultsets <- renderValueBox(
     valueBox(
-      value = get("mobile_dygraph_means", envir = data_env)[2],
+      value = mobile_dygraph_means[2],
       subtitle = "Result sets per day",
       icon = icon("list", lib = "glyphicon"),
       color = "green"
@@ -131,7 +118,7 @@ shinyServer(function(input, output) {
   
   output$mobile_event_clickthroughs <- renderValueBox(
     valueBox(
-      value = get("mobile_dygraph_means", envir = data_env)[1],
+      value = mobile_dygraph_means[1],
       subtitle = "Clickthroughs per day",
       icon = icon("hand-up", lib = "glyphicon"),
       color = "green"
@@ -140,18 +127,18 @@ shinyServer(function(input, output) {
   
   #Mobile plots
   output$mobile_event_plot <- make_dygraph(
-    get("mobile_dygraph_set", envir = data_env), "Date", "Events",
+    mobile_dygraph_set, "Date", "Events",
     "Mobile search events, by day"
   )
   output$mobile_load_plot <- make_dygraph(
-    get("mobile_load_data", envir = data_env), "Date", "Load time (ms)",
+    mobile_load_data, "Date", "Load time (ms)",
     "Mobile result load times, by day"
   )
   
   #App value boxes
   output$app_event_searches <- renderValueBox(
     valueBox(
-      value = get("app_dygraph_means", envir = data_env)[3],
+      value = app_dygraph_means[3],
       subtitle = "Search sessions per day",
       icon = icon("search"),
       color = "green"
@@ -160,7 +147,7 @@ shinyServer(function(input, output) {
   
   output$app_event_resultsets <- renderValueBox(
     valueBox(
-      value = get("app_dygraph_means", envir = data_env)[2],
+      value = app_dygraph_means[2],
       subtitle = "Result sets per day",
       icon = icon("list", lib = "glyphicon"),
       color = "green"
@@ -169,7 +156,7 @@ shinyServer(function(input, output) {
   
   output$app_event_clickthroughs <- renderValueBox(
     valueBox(
-      value = get("app_dygraph_means", envir = data_env)[1],
+      value = app_dygraph_means[1],
       subtitle = "Clickthroughs per day",
       icon = icon("hand-up", lib = "glyphicon"),
       color = "green"
@@ -178,17 +165,17 @@ shinyServer(function(input, output) {
   
   #App plots
   output$app_event_plot <- make_dygraph(
-    get("app_dygraph_set", envir = data_env), "Date", "Events",
+    app_dygraph_set, "Date", "Events",
     "Mobile App search events, by day"
   )
   output$app_load_plot <- make_dygraph(
-    get("app_load_data", envir = data_env), "Date", "Load time (ms)",
+    app_dygraph_set, "Date", "Load time (ms)",
     "Mobile App result load times, by day"
   )
   
   #Failure plots
   output$failure_rate_plot <- make_dygraph(
-    get("failure_dygraph_set", envir = data_env), "Date", "Queries",
+    failure_dygraph_set, "Date", "Queries",
     "Search Queries with Zero Results, by day"
   )
 })
