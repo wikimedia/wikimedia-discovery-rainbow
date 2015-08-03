9848c1e40755c819c76612626f76fe538be9b286
Mikhail Popov <mikhail@mpopov.com>
2015-08-03 14:12:36 -0700

    Minor fix for KPI summary header
    Corrected the order of dates

--- server.R
+++ server.R
@@ -266,7 +266,7 @@ shinyServer(function(input, output) {
              sub("([a-z]{2})", "<sup>\\1</sup>",
                  sapply(as.numeric(as.character(., "%e")), toOrdinal)))
     } %>%
-      { sprintf("<h3>KPI summary for %s, and %% change from %s:</h3>", .[1], .[2]) } %>%
+      { sprintf("<h3>KPI summary for %s, and %% change from %s:</h3>", .[2], .[1]) } %>%
       HTML()
   })
   output$kpi_summary_load_time <- renderValueBox({
